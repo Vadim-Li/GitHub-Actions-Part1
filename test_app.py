7c9229af9b699fd37d8980beecab2d1d5b600afd
Vadim-Li <125968631+Vadim-Li@users.noreply.github.com>
2025-03-09 21:53:54 +0800

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,3 +1,4 @@
+import unittest
 from app import add, subtract
 
 def test_add():
@@ -5,3 +6,14 @@ def test_add():
 
 def test_subtract():
     assert subtract(5, 3) == 2
+
+
+class TestApp(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
+
+    def test_subtract(self):
+        self.assertEqual(subtract(5, 3), 2)
+
+if __name__ == "__main__":
+    unittest.main()
